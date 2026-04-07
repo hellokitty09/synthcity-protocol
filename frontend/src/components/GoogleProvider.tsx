6e31e0ebab2935bf3ca02ec15e020d2efcb56d78
hellokitty09 <183717166+hellokitty09@users.noreply.github.com>
2026-04-07 22:42:40 +0530

    security: remove hardcoded client ID out of an abundance of caution

--- frontend/src/components/GoogleProvider.tsx
+++ frontend/src/components/GoogleProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const CLIENT_ID = "46563670881-97ib4shudphbp9adq4ru8sfk4dbj964v.apps.googleusercontent.com";
+const CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "";
 
 export default function GoogleProvider({ children }: { children: React.ReactNode }) {
   if (!CLIENT_ID) return <>{children}</>;
